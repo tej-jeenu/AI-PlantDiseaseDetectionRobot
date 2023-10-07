0430ae236988e06de35a1282c80ed70278c4a979
tej-jeenu <130780124+tej-jeenu@users.noreply.github.com>
2023-10-07 14:05:13 +0100

    Update train.py

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import cv2
 
 model = YOLO("yolov8s.pt")
 #model = YOLO("best copy.pt")
-model.train(data="datasets\yoloplantdataset\data.yaml", epochs=5, imgsz=640, workers=8, batch=16)
+model.train(data="datasets\yoloplantdataset\data.yaml", epochs=50, imgsz=640, workers=8, batch=16)
 #results = model.predict(source="flowervid.mp4", show=True)
 
 #print(results)
